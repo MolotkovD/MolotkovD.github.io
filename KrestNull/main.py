185cb5e2126c05e385b5edb52a0737d77f67ba56
MolotkovD <MolotkovD@users.noreply.github.com>
2021-06-01 22:58:11 +0300

    1

--- KrestNull/main.py
+++ KrestNull/main.py
@@ -22,26 +22,34 @@ queue_player = "cross"
 
 def wining():
     win_game = False
-
+    status = None
     if data["score"]["1"] == data["score"]["2"] == data["score"]["3"] and data["rules"]["1"] == data["rules"]["2"] == data["rules"]["3"] == False:
         win_game = True
+        status = data["score"]["1"]
     if data["score"]["4"] == data["score"]["5"] == data["score"]["6"] and data["rules"]["4"] == data["rules"]["5"] == data["rules"]["6"] == False:
         win_game = True
+        status = data["score"]["4"]
     if data["score"]["7"] == data["score"]["8"] == data["score"]["9"] and data["rules"]["7"] == data["rules"]["8"] == data["rules"]["9"] == False:
         win_game = True
+        status = data["score"]["7"]
     if data["score"]["1"] == data["score"]["4"] == data["score"]["7"] and data["rules"]["1"] == data["rules"]["4"] == data["rules"]["7"] == False:
         win_game = True
+        status = data["score"]["1"]
     if data["score"]["2"] == data["score"]["5"] == data["score"]["8"] and data["rules"]["2"] == data["rules"]["5"] == data["rules"]["8"] == False:
         win_game = True
+        status = data["score"]["2"]
     if data["score"]["3"] == data["score"]["6"] == data["score"]["9"] and data["rules"]["3"] == data["rules"]["6"] == data["rules"]["9"] == False:
         win_game = True
+        status = data["score"]["3"]
     if data["score"]["3"] == data["score"]["5"] == data["score"]["7"] and data["rules"]["3"] == data["rules"]["5"] == data["rules"]["7"] == False:
         win_game = True
+        status = data["score"]["3"]
     if data["score"]["1"] == data["score"]["5"] == data["score"]["9"] and data["rules"]["1"] == data["rules"]["5"] == data["rules"]["9"] == False:
         win_game = True
+        status = data["score"]["1"]
     ret = pygame.Rect(615, 100, 200, 100)
     pygame.draw.rect(win, (0, 0, 0), ret)
-    score = fon.render(f"Win = {win_game}", True, (255, 255, 255))
+    score = fon.render(f"Win = {status}", True, (255, 255, 255))
     win.blit(score, (615, 100))
     pygame.display.update()
 def queue(x, y, fig):
